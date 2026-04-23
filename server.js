29c2d60d7c6b6443045179e0ce0ddcd216759efd
Acepae <acepae88@gmail.com>
2026-04-23 19:56:29 +0900

    PGB 선물하기 첫 배포

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Server running: http://localhost:' + PORT);
+});
